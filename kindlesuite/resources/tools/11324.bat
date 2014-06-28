2b079520758bfb851211eba2d91e30797f11a3d3
kyleberries <kyleberry.graphics@gmail.com>
2014-06-28 16:55:06 -0500

    11324 root

---
+++ kindlesuite/resources/tools/11324.bat
@@ -0,0 +1,26 @@
+@echo off
+echo Make sure you power Kindle OFF and plug into FASTBOOT cable
+pause
+echo.
+echo.
+echo.
+echo Downgrading Boot Image...
+fastboot -i 0x1949 flash boot resources/boot/11310-boot.img
+fastboot -i 0x1949 continue
+adb wait-for-device
+echo Device connected. Pushing files...
+adb push resources/root/su /data/local/tmp/
+adb push resources/root/exploit /data/local/tmp/
+adb push resources/root/rootme.sh /data/local/tmp/
+echo Changing permissions...
+adb shell chmod 755 /data/local/tmp/*
+echo Executing Exploit (could take some time, be patient!)
+pause
+adb shell /data/local/tmp/exploit -c "/data/local/tmp/rootme.sh"
+echo.
+echo.
+echo rebooting fastboot...
+adb reboot
+fastboot -i 0x1949 wait-for-device devices
+fastboot -i 0x1949 flash boot resources/boot/11324-boot.img
+fastboot -i 0x1949 continue
