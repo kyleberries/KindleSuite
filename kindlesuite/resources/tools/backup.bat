35f95d5311a8a8b2d728fd8835ac6e0a2f25cf83
kyleberries <kyleberry.graphics@gmail.com>
2014-07-03 15:15:56 -0500

    3.1
    adb driver update, adb backup correction

--- kindlesuite/resources/tools/backup.bat
+++ kindlesuite/resources/tools/backup.bat
@@ -1,8 +1,7 @@
 @echo off
 mkdir resources\backup
-cd resources\backup
 echo This will backup the ENTIRE device (except system), so be prepared for a long wait. this screen will inform you when the backup is complete
-adb backup -apk -shared -all -nosystem
+adb backup -apk -shared -all -nosystem -f .\resources\backup\backup.ab
 echo backup complete
 pause
 exit
