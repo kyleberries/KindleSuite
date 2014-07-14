797d34aac8571335928602c1f651be571268eae4
locino <locino@users.noreply.github.com>
2014-07-14 23:54:16 +0200

    Delete backup.cmd

--- resources/scripts/backup/backup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo This will backup the ENTIRE device (except system), so be prepared for a long wait. this screen will inform you when the backup is complete
-resources\tools\adb backup -apk -shared -all -nosystem -f resources\scripts\backup\backup.ab
-echo backup complete
-pause
-exit
