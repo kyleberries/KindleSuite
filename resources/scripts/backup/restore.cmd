165935602be9ac7b820b092c2c12a73ef186f491
locino <locino@users.noreply.github.com>
2014-07-14 23:54:21 +0200

    Delete restore.cmd

--- resources/scripts/backup/restore.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo This will restore the ENTIRE device, so be prepared for a long wait. this screen will inform you when the restore is complete
-resources\tools\adb restore resources\scripts\backup\backup.ab
-echo restore complete
-del resources\scripts\backup\backup.ab
-pause
-exit
