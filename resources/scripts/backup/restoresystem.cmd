56a0ab0aec19f556ec141b08f58100ef4145cde0
locino <locino@users.noreply.github.com>
2014-07-14 23:53:54 +0200

    Create restoresystem.cmd

---
+++ resources/scripts/backup/restoresystem.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo do not restore system, I think it can softbrick you rom.
+#echo This will restore the system parttition, so be prepared for a long wait. this screen will inform you when the restore is complete
+#resources\tools\adb restore resources\scripts\backup\backupsystem.ab
+#echo restore complete
+#del resources\scripts\backup\backupsystem.ab
+pause
+exit
