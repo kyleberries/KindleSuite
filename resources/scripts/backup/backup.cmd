29668c34bfb3e5120461dc4fa2bcd0fbe7c2bf8f
kyleberries <kyleberry.graphics@gmail.com>
2014-07-15 09:23:28 -0500

    testing update
    ks4 testing

--- resources/scripts/backup/backup.cmd
+++ resources/scripts/backup/backup.cmd
@@ -2,5 +2,4 @@
 echo This will backup the ENTIRE device (except system), so be prepared for a long wait. this screen will inform you when the backup is complete
 resources\tools\adb backup -apk -shared -all -nosystem -f resources\scripts\backup\backup.ab
 echo backup complete
-pause
 exit
---
+++ resources/scripts/backup/backupdel.cmd
@@ -0,0 +1,2 @@
+del resources\scripts\backup\backup.ab
+exit
