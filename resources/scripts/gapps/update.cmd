628c38f0dd573d76fbe7ee7d3353b1c2587511fc
locino <locino@users.noreply.github.com>
2014-07-19 23:22:35 +0200

    Update update.cmd

--- resources/scripts/gapps/update.cmd
+++ resources/scripts/gapps/update.cmd
@@ -1,4 +1,5 @@
 @echo off
+
 echo Prepairing directories...>resources\console.txt
 if not exist resources\scripts\gapps mkdir resources\scripts\gapps
 if not exist resources\scripts\gapps\installer mkdir resources\scripts\gapps\installer
@@ -6,7 +7,7 @@ if not exist resources\scripts\gapps\checksums mkdir resources\scripts\gapps\che
 if not exist resources\scripts\gapps\package mkdir resources\scripts\gapps\package
 
 echo Updating installer...>resources\console.txt
-del resources\scripts\gapps\gapps.cmd
+del resources\scripts\gapps\installer\gapps.cmd
 resources\tools\wget -P resources/scripts/gapps/installer https://raw.githubusercontent.com/secretdevwork/KindleSuite/master/resources/scripts/gapps/gapps.cmd
 
 
@@ -14,7 +15,6 @@ echo Prepairing Gapps...>resources\console.txt
 if not exist resources\scripts\gapps\package\gapps.zip resources\tools\wget -P resources/scripts/gapps/package http://d-h.st/udB
 del resources\scripts\gapps\checksums\MD5
 del resources\scripts\gapps\checksums\MD5local
-del resources\scripts\gapps\checksums\MD5server
 resources\tools\wget -P resources/scripts/gapps/checksums https://raw.githubusercontent.com/secretdevwork/KindleSuite/master/updater/gapps/MD5
 if not exist resources\scripts\gapps\checksums\MD5local resources\tools\md5sums -u resources\scripts\gapps\package\gapps.zip > resources\scripts\gapps\checksums\MD5local
 set /p md5=<resources\scripts\gapps\checksums\MD5
@@ -26,4 +26,3 @@ exit )
 
 echo Extracting Gapps...>resources\console.txt
 if not exist resources\scripts\gapps\package\gapps resources\tools\FBZip -e resources\scripts\gapps\package\gapps.zip resources\scripts\gapps\package\
-
