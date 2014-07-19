1d4db6471521bc1d33f68735183241898af20e62
locino <locino@users.noreply.github.com>
2014-07-19 23:38:53 +0200

    cleaning up and reformating

--- resources/scripts/gapps/gapps.cmd
+++ resources/scripts/gapps/gapps.cmd
@@ -1,44 +1,31 @@
 @echo off
-mkdir resources\gapps
-echo Downloading ROM...
-if not exist resources\scripts\gapps\gapps.zip wget -P resources/gapps http://d-h.st/udB
-echo.
-echo.
-echo Unzipping ROM...
-if not exist resources\scripts\gapps\files\Lucky-Patcher-v4.4.3.apk FBZip -e -p resources/scripts/gapps/gapps.zip resources/scripts/gapps/files/
-del resourcs\scripts\*.zip
-echo Plug kindle into regular cable with ADB Debugging enabled...
+echo Plug kindle into regular cable with ADB Debugging enabled...>resources\console.txt
 resources\tools\adb wait-for-device
-echo.
-echo Installing Lucky Patcher...
-resources\tools\adb install resources\scripts\gapps\files\Lucky-Patcher-v4.4.3.apk
-echo.
-echo Making temporary directories...
+
+echo Installing Lucky Patcher...>resources\console.txt
+resources\tools\adb install resources\scripts\gapps\package\gapps\Lucky-Patcher-v4.4.3.apk
+
+echo Prepairing directories...>resources\console.txt
 resources\tools\adb shell mkdir /sdcard/appstore
-resources\tools\adb shell mkdir /sdcard/bin
 resources\tools\adb shell mkdir /sdcard/gapps
 resources\tools\adb shell mkdir /sdcard/lib
 resources\tools\adb shell mkdir /sdcard/framework
 resources\tools\adb shell mkdir /sdcard/etc
 resources\tools\adb shell mkdir /sdcard/etc/permissions 
-echo.
-echo Pushing files to SD...
-resources\tools\adb push resources\scripts\gapps\files\appstore /sdcard/appstore
-resources\tools\adb push resources\scripts\gapps\files\bin /sdcard/bin
-resources\tools\adb push resources\scripts\gapps\files\gapps /sdcard/gapps
-resources\tools\adb push resources\scripts\gapps\files\lib /sdcard/lib
-resources\tools\adb push resources\scripts\gapps\files\framework /sdcard/framework
-resources\tools\adb push resources\scripts\gapps\files\etc\permissions /sdcard/etc/permissions
-echo.
-echo.
-echo Copying files to System and set permissions...
+
+echo Copying files...>resources\console.txt
+resources\tools\adb push resources\scripts\gapps\package\gapps\appstore /sdcard/appstore
+resources\tools\adb push resources\scripts\gapps\package\gapps\gapps /sdcard/gapps
+resources\tools\adb push resources\scripts\gapps\package\gapps\lib /sdcard/lib
+resources\tools\adb push resources\scripts\gapps\package\gapps\framework /sdcard/framework
+resources\tools\adb push resources\scripts\gapps\package\gapps\etc\g.prop /sdcard/etc/
+resources\tools\adb push resources\scripts\gapps\package\gapps\etc\permissions /sdcard/etc/permissions
+
+echo Installing and setting permissions...>resources\console.txt
 resources\tools\adb shell su -c "mount -o remount rw, /system"
 resources\tools\adb shell su -c "mkdir /system/dlpc"
 resources\tools\adb shell su -c "cp /system/app/DownloadProvider*.* /system/dlpc"
 resources\tools\adb shell su -c "rm /system/app/DownloadProvider*.*"
-resources\tools\adb shell su -c "cp /sdcard/bin/* /system/bin/"
-resources\tools\adb shell su -c "chown 0.2000 /system/bin/asi"
-resources\tools\adb shell su -c "chmod 755 /system/bin/asi"
 resources\tools\adb shell su -c "cp /sdcard/gapps/* /system/app/"
 resources\tools\adb shell su -c "chown 0.0 /system/app/*"
 resources\tools\adb shell su -c "chmod 644 /system/app/*"
@@ -51,29 +38,17 @@ resources\tools\adb shell su -c "chmod 644 /system/framework/*"
 resources\tools\adb shell su -c "cp /sdcard/etc/permissions/* /system/etc/permissions/"
 resources\tools\adb shell su -c "chown 0.0 /system/etc/permissions/*"
 resources\tools\adb shell su -c "chmod 644 /system/etc/permissions/*"
-echo.
-echo.
-echo wait
-resources\tools\adb shell su -c "/system/bin/asi"
-echo.
-echo removing temp files...
+
+echo Cleaning up...>resources\console.txt
 resources\tools\adb shell rm -rf /sdcard/appstore
 resources\tools\adb shell rm -rf /sdcard/gapps
 resources\tools\adb shell rm -rf /sdcard/lib
 resources\tools\adb shell rm -rf /sdcard/framework
 resources\tools\adb shell rm -rf /sdcard/etc
-resources\tools\adb shell rm -rf /sdcard/etc/permissions
-resources\tools\adb shell su -c "rm -f /system/bin/asi"
-echo.
-echo unmounting
 resources\tools\adb shell su -c "mount -o remount ro, /system"
-echo successfull unmounted
-echo.
-echo.
-echo File copy complete, open Lucky Patcher on Kindle and select toolbox at bottom.
-echo Select patch to android. 
-echo Select disable signature verification in pkg manager.
-echo Congratulations, You now have Google Play Store!
-echo ONLY NOT STOCK ROM: In order to use the included Amazon Appstore, please do not update the app.
+echo Successfull installed>resources\console.txt
+
+echo Open Lucky Patcher on Kindle and select toolbox at bottom...>resources\console.txt
+echo Select patch to android. Select disable signature verification in pkg manager...>resources\console.txt
 pause
 exit
