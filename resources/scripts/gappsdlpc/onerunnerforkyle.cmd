42237cf67b153df5a70e3d12ac12adad4020c6b4
locino <locino@users.noreply.github.com>
2014-07-20 01:38:53 +0200

    Delete onerunnerforkyle.cmd

--- resources/scripts/gappsdlpc/onerunnerforkyle.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Plug Kindle into regular cable with ADB Debugging enabled...>resources\console.txt
-resources\tools\adb wait-for-device
-
-echo Preparing Gapps...>resources\console.txt
-start resources/scripts/gappsdlpc/updategapps.cmd
-pause
-exit
