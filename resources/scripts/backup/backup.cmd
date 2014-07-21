2e818d200311b687303b5e900f3ac60ac755dc70
kyleberries <kyleberry.graphics@gmail.com>
2014-07-21 07:23:52 -0500

    testing update

--- resources/scripts/backup/backup.cmd
+++ resources/scripts/backup/backup.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo This will backup the ENTIRE device (except system), so be prepared for a long wait. this screen will inform you when the backup is complete>resources\console.txt
-resources\tools\adb backup -apk -shared -all -nosystem -f resources\scripts\backup\backupdata.ab
+resources\tools\adb backup -apk -shared -all -nosystem -f resources\scripts\backup\backup.ab
 echo backup complete>resources\console.txt
 exit
