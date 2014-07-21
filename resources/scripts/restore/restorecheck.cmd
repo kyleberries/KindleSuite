79b47f38e083e9dab4a7e563cfaed67fba047870
kyleberries <kyleberry.graphics@gmail.com>
2014-07-21 08:30:06 -0500

    reset console/cancel combined

--- resources/scripts/restore/restorecheck.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-if exist resources\scripts\restore\update.zip start resources\scripts\restore\restore.bat
-if not exist resources\scripts\restore\update.zip start resources\scripts\restore\restoredl.bat
-exit
