d5b94e7077037f2fd56080b014f5ac479092b3e0
locino <locino@users.noreply.github.com>
2014-07-22 03:32:24 +0200

    Update runme.cmd

--- resources/scripts/gapps/runme.cmd
+++ resources/scripts/gapps/runme.cmd
@@ -1,11 +1,15 @@
 @echo off
 
+resources\tools\adb start-server
+
 echo Updating updater...>resources\console.txt
 del resources\scripts\gapps\updater\update.cmd
 resources\tools\wget -P resources/scripts/gapps/updater https://raw.githubusercontent.com/secretdevwork/KindleSuite/master/resources/scripts/gapps/updater/update.cmd
 
 echo Starting updater...>resources\console.txt
 start resources\scripts\gapps\updater\update.cmd
+
+resources\tools\adb kill-server
 
 pause
 exit
