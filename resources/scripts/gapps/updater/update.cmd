368965912404b7b8642c16d6bd902d5aa83e9e7c
locino <locino@users.noreply.github.com>
2014-07-22 03:33:34 +0200

    Update update.cmd

--- resources/scripts/gapps/updater/update.cmd
+++ resources/scripts/gapps/updater/update.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+resources\tools\adb start-server
+
 echo Plug kindle into regular cable with ADB Debugging enabled...>resources\console.txt
 resources\tools\adb wait-for-device
 
@@ -30,6 +32,8 @@ if not exist resources\scripts\gapps\package\gapps resources\tools\FBZip -e reso
 
 echo Starting installer...>resources\console.txt
 start resources\scripts\gapps\installer\gapps.cmd
+
+resources\tools\adb kill-server
 
 pause
 exit
