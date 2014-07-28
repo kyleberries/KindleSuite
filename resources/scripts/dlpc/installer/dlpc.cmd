ef3154c88a4a095a6f9bdcb290391f407cbc56c9
locino <locino@users.noreply.github.com>
2014-07-28 19:22:44 +0200

    Update dlpc.cmd
    little update

--- resources/scripts/dlpc/installer/dlpc.cmd
+++ resources/scripts/dlpc/installer/dlpc.cmd
@@ -12,25 +12,18 @@ resources\tools\adb push resources\scripts\dlpc\package\hotreboot.x /sdcard/dlpc
 
 echo Installing and setting permissions...>resources\console.txt
 resources\tools\adb shell su -c "mount -o remount,rw /system"
-resources\tools\adb shell su -c "cp /sdcard/dlpc/* /system/dlpc"
-resources\tools\adb shell su -c "mv /system/dlpc/dlpc.x /system/dlpc/dlpc"
-resources\tools\adb shell su -c "mv /system/dlpc/dlp.x /system/dlpc/dlp"
-resources\tools\adb shell su -c "mv /system/dlpc/hotreboot.x /system/dlpc/hotreboot"
-resources\tools\adb shell su -c "chown 0.2000 /system/dlpc/dlpc"
-resources\tools\adb shell su -c "chown 0.2000 /system/dlpc/dlp"
-resources\tools\adb shell su -c "chown 0.2000 /system/dlpc/hotreboot"
-resources\tools\adb shell su -c "chmod 755 /system/dlpc/dlpc"
-resources\tools\adb shell su -c "chmod 755 /system/dlpc/dlp"
-resources\tools\adb shell su -c "chmod 755 /system/dlpc/hotreboot"
+resources\tools\adb shell su -c "mv /sdcard/dlpc/dlpc.x /system/bin/dlpc"
+resources\tools\adb shell su -c "mv /sdcard/dlpc/dlp.x /system/bin/dlp"
+resources\tools\adb shell su -c "mv /sdcard/dlpc/hotreboot.x /system/bin/hotreboot"
+resources\tools\adb shell su -c "chown 0.2000 /system/bin/dlpc"
+resources\tools\adb shell su -c "chown 0.2000 /system/bin/dlp"
+resources\tools\adb shell su -c "chown 0.2000 /system/binc/hotreboot"
+resources\tools\adb shell su -c "chmod 755 /system/bin/dlpc"
+resources\tools\adb shell su -c "chmod 755 /system/bin/dlp"
+resources\tools\adb shell su -c "chmod 755 /system/bin/hotreboot"
 
 echo Cleaning up...>resources\console.txt
 resources\tools\adb shell rm -rf /sdcard/dlpc
-resources\tools\adb shell su -c "rm /system/bin/dlpc"
-resources\tools\adb shell su -c "cp /system/dlpc/dlpc /system/bin"
-resources\tools\adb shell su -c "rm /system/bin/dlp"
-resources\tools\adb shell su -c "cp /system/dlpc/dlp /system/bin"
-resources\tools\adb shell su -c "rm /system/bin/hotreboot"
-resources\tools\adb shell su -c "cp /system/dlpc/hotreboot /system/bin"
 resources\tools\adb shell su -c "mount -o remount,ro /system"
 echo Successfull installed>resources\console.txt
 
