091ba44493aba0177f2f78f475d1952f46e365eb
locino <locino@users.noreply.github.com>
2014-07-30 02:48:10 +0200

    Update hosts.cmd

--- resources/scripts/mods/hosts/installer/hosts.cmd
+++ resources/scripts/mods/hosts/installer/hosts.cmd
@@ -11,8 +11,22 @@ resources\tools\adb push resources\scripts\mods\hosts\package\hosts.x /sdcard/ho
 echo Installing and setting permissions...>resources\console.txt
 resources\tools\adb shell su -c "mount -o remount,rw /system"
 resources\tools\adb shell su -c "cp /sdcard/hosts/hosts.x /system/etc/hosts"
-#resources\tools\adb shell su -c "chown 0.0 /system/etc/hosts"
-#resources\tools\adb shell su -c "chmod 644 /system/etc/hosts"
+resources\tools\adb shell su -c "chown 0.0 /system/etc/hosts"
+resources\tools\adb shell su -c "chmod 644 /system/etc/hosts"
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAUpdateMessageHandler""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.InventoryUploadService""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.UpdatesLookupService""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAService""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.AppStateMonitorService""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.CheckForOTAUpdatesService""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.ViewUpdateActivity""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAStatusActivity""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.ResetOTAMessageHandler""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.InventoryUploadService$InventoryUploadReceiver""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.UpdatesLookupService$MountStatusChangedReceiver""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAService$OTAWakeupReceiver""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAService$OTADedupeReceiver""
+resources\tools\adb shell su -c "pm disable "com.amazon.dcp/.ota.OTAService$OTAInstallNowReceiver""
 
 echo Cleaning up...>resources\console.txt
 resources\tools\adb shell rm -rf /sdcard/hosts
