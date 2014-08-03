179f79c1ea2dd190d5e5e18b143d8ce1b28ef31d
kyleberries <kyleberry.graphics@gmail.com>
2014-08-03 18:22:10 -0500

    AppJS overhaul

---
+++ data/KStest.js
@@ -0,0 +1,63 @@
+// load appjs
+
+var appjs = require('appjs'),osutils=require('./lib/osutils.js');
+
+// serve static files from a directory
+appjs.serveFilesFrom(__dirname + '/content');
+
+// handle requests from the browser
+/*appjs.post('/', function(request, response, next){
+  response.send('Hey! How are you ' + request.post('firstname'));
+})*/
+
+// create a window
+var window = appjs.createWindow({
+  width: 800,
+  height: 600,
+  alpha:true,
+  icons  : __dirname + '/content/icons',
+  showChrome:true
+});
+
+// prepare the window when first created
+window.on('create', function(){
+  console.log("Window Created");
+  // window.frame controls the desktop window
+  window.frame.show().center();
+});
+
+// the window is ready when the DOM is loaded
+window.on('ready', function(){
+  console.log("Window Ready");
+  
+  window.sysinfo=osutils;
+  
+  window.commands={
+	maximize:function(){
+		window.frame.maximize();
+		return 'restore';
+	},restore:function(){
+		window.frame.restore();
+		return 'maximize';
+	},minimize:function(){
+		window.frame.minimize();
+		return 'minimize';
+	},close:function(){
+		window.frame.destroy();
+		return 'close';
+	}
+  };
+
+  window.addEventListener('keydown', function(e){
+    // show chrome devtools on f12 or commmand+option+j
+    if (e.keyIdentifier === 'F12' || e.keyCode === 74 && e.metaKey && e.altKey) {
+      window.frame.openDevTools();
+    }
+	if(e.keyIdentifier === 'F5') window.location.reload();
+  });
+});
+
+// cleanup code when window is closed
+window.on('close', function(){
+  console.log("Window Closed");
+});
---
+++ data/content/scripts/navigation.js
@@ -0,0 +1,6 @@
+function adbBtn() {
+var home = document.getElementById('home') ;
+var drivers = document.getElementById('drivers') ;
+home.style.display='none';
+drivers.style.display='block';
+} 
