fa5576f7c0f2dfa32f7e3604acd93c0cb98a08ba
kyleberries <kyleberry.graphics@gmail.com>
2014-08-04 16:14:38 -0500

    static nav

--- data/content/scripts/jquery.js
+++ data/content/scripts/jquery.js
@@ -2,8 +2,3 @@ $(document).ready(function(){
     $("html").fadeIn(3000);
   });
   
-  $(document).ready(function(){
-  $("a.link").hover(function(){
-    $("this").css("background-color","grey");
-  });
-});
