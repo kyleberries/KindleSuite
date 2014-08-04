64e22a21fc993bfe99e5e81b90bb3a8c80edffa6
kyleberries <kyleberry.graphics@gmail.com>
2014-08-03 21:06:12 -0500

    jq update

--- data/content/scripts/jquery.js
+++ data/content/scripts/jquery.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $("html").fadeIn(3000);
   });
+  
+  $(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
