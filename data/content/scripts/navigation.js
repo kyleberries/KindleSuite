8ed31923ff7ca3878b054610ea959cbac05d9ac7
kyleberries <kyleberry.graphics@gmail.com>
2014-08-04 13:41:39 -0500

    JQ anims

--- data/content/scripts/navigation.js
+++ data/content/scripts/navigation.js
@@ -1,96 +1,56 @@
-function driverBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='block';
-root.style.display='none';
-restore.style.display='none';
-gapps.style.display='none';
-roms.style.display='none';
-extras.style.display='none';
-} 
-function rootBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='none';
-root.style.display='block';
-restore.style.display='none';
-gapps.style.display='none';
-roms.style.display='none';
-extras.style.display='none';
-} 
-function restoreBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='none';
-root.style.display='none';
-restore.style.display='block';
-gapps.style.display='none';
-roms.style.display='none';
-extras.style.display='none';
-} 
-function gappsBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='none';
-root.style.display='none';
-restore.style.display='none';
-gapps.style.display='block';
-roms.style.display='none';
-extras.style.display='none';
-} 
-function romsBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='none';
-root.style.display='none';
-restore.style.display='none';
-gapps.style.display='none';
-roms.style.display='block';
-extras.style.display='none';
-} 
-function extrasBtn() {
-var home = document.getElementById('home') ;
-var drivers = document.getElementById('drivers') ;
-var root = document.getElementById('root') ;
-var restore = document.getElementById('restore') ;
-var gapps = document.getElementById('gapps') ;
-var roms = document.getElementById('roms') ;
-var extras = document.getElementById('extras') ;
-home.style.display='none';
-drivers.style.display='none';
-root.style.display='none';
-restore.style.display='none';
-gapps.style.display='none';
-roms.style.display='none';
-extras.style.display='block';
-} 
+$(document).ready(function(){
+  $("#driverBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").show(1000);
+    $("#root").hide(1000);
+    $("#restore").hide(1000);
+    $("#gapps").hide(1000);
+    $("#roms").hide(1000);
+    $("#extras").hide(1000);
+  });
+   $("#rootBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").hide(1000);
+    $("#root").show(1000);
+    $("#restore").hide(1000);
+    $("#gapps").hide(1000);
+    $("#roms").hide(1000);
+    $("#extras").hide(1000);
+  });
+   $("#restoreBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").hide(1000);
+    $("#root").hide(1000);
+    $("#restore").show(1000);
+    $("#gapps").hide(1000);
+    $("#roms").hide(1000);
+    $("#extras").hide(1000);
+  });
+   $("#gappsBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").hide(1000);
+    $("#root").hide(1000);
+    $("#restore").hide(1000);
+    $("#gapps").show(1000);
+    $("#roms").hide(1000);
+    $("#extras").hide(1000);
+  });
+   $("#romsBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").hide(1000);
+    $("#root").hide(1000);
+    $("#restore").hide(1000);
+    $("#gapps").hide(1000);
+    $("#roms").show(1000);
+    $("#extras").hide(1000);
+  });
+   $("#extrasBtn").click(function(){
+    $("#home").hide(1000);
+    $("#drivers").hide(1000);
+    $("#root").hide(1000);
+    $("#restore").hide(1000);
+    $("#gapps").hide(1000);
+    $("#roms").hide(1000);
+    $("#extras").show(1000);
+  });
+});
