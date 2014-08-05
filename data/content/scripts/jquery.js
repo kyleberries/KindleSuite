2cefbeb324aad3ab507197ba25e395f364d88583
kyleberries <kyleberry.graphics@gmail.com>
2014-08-05 18:29:35 -0500

    increase snap

--- data/content/scripts/jquery.js
+++ data/content/scripts/jquery.js
@@ -1,64 +1,63 @@
 $(document).ready(function(){
- $("html").fadeIn(500);
+ $("html").fadeIn(150);
  $(".title1").click(function(){
-          $("html").fadeOut(300);
+          $("html").fadeOut(150);
           location.reload();
 	                         });
- $(".title1").fadeOut(0);
- $(".title1").load($(".title1").fadeIn(2000));
+ $(".title1").load($(".title1").fadeIn(1500));
  $(".title2").fadeOut(0);
- $(".title2").load($(".title2").fadeIn(2000));
+ $(".title2").load($(".title2").fadeIn(1500));
    $("#driverBtn").click(function(){
-    $("div.content").fadeOut(500);
-    $("#drivers").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);
+    $("#drivers").delay(150).fadeIn(150);
   });
    $("span.close").click(function(){
     window.close();
 	});
 	  $(".close").hover(function(){
-    $("html").fadeTo(200, .5);
+    $("html").fadeTo(100, .5);
     },function(){
-    $("html").fadeTo(500,.98);
+    $("html").fadeTo(50,.98);
   });
    $("#rootBtn").click(function(){
-    $("div.content").fadeOut(500);
-    $("#root").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);
+    $("#root").delay(150).fadeIn(150);
   });
    $("#restoreBtn").click(function(){
-    $("div.content").fadeOut(500);   
-    $("#restore").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);   
+    $("#restore").delay(150).fadeIn(150);
   });
    $("#gappsBtn").click(function(){
-    $("div.content").fadeOut(500);
-    $("#gapps").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);
+    $("#gapps").delay(150).fadeIn(150);
   });
    $("#romsBtn").click(function(){
-    $("div.content").fadeOut(500);
-    $("#roms").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);
+    $("#roms").delay(150).fadeIn(150);
   });
    $("#extrasBtn").click(function(){
-    $("div.content").fadeOut(500);
-    $("#extras").delay(500).fadeIn(500);
+    $("div.content").fadeOut(150);
+    $("#extras").delay(150).fadeIn(150);
   });
    $("#ice_show").click(function(){
-    $("#hellfire").fadeOut(500);
-	$("#plasma").fadeOut(500);
-    $("#ice").delay(500).fadeIn(500);
+    $("#hellfire").fadeOut(150);
+	$("#plasma").fadeOut(150);
+    $("#ice").delay(150).fadeIn(150);
   });
      $("#hellfire_show").click(function(){
-    $("#plasma").fadeOut(500);
-	$("#ice").fadeOut(500);
-    $("#hellfire").delay(500).fadeIn(500);
+    $("#plasma").fadeOut(150);
+	$("#ice").fadeOut(150);
+    $("#hellfire").delay(150).fadeIn(150);
   });
      $("#plasma_show").click(function(){
-    $("#hellfire").fadeOut(500);
-	$("#ice").fadeOut(500);
-    $("#plasma").delay(500).fadeIn(500);
+    $("#hellfire").fadeOut(150);
+	$("#ice").fadeOut(150);
+    $("#plasma").delay(150).fadeIn(150);
   });
      $("div.navigation").hover(function(){
-    $("div.navigation").fadeTo(200, .7);
+    $("div.navigation").fadeTo(150, .7);
     },function(){
-    $("div.navigation").fadeTo(200, 0.5);
+    $("div.navigation").fadeTo(150, 0.5);
   });
 
 });
