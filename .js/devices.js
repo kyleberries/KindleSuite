f1867c886044b467b2a363207ea074eeac99479e
kyleberries <kyleberry.graphics@gmail.com>
2014-08-16 13:57:18 -0500

    GUI killed. frame added

--- .js/devices.js
+++ .js/devices.js
@@ -4,8 +4,16 @@ var Promise = require('bluebird');
 
 function checkDevices(){
 client.listDevices().then(function(devices){
-if(devices.length != 0) {alert('Device Detected!')}
-else {alert('NO Device Detected! Click KindleSuite title to check again.')}
+if(devices.length != 0) {
+/*grep ro.product.model from build.prop to var model
+ If (model != "ro.product.model=KFSOWI" && model.length != 0) {alert('KindleSuite does not work with this device')}
+ else {continue}
+ */
+alert('Device Detected!')
+}
+else {
+alert('NO Device Detected! Click KindleSuite title to check again.')
+}
 client.kill(device);
 })
 };
