a28c45ea81eceb210775401263369ee758f8951e
kyleberries <kyleberry.graphics@gmail.com>
2014-08-16 15:44:01 -0500

    new nw

---
+++ .js/pages.js
@@ -0,0 +1,14 @@
+var home = 
+"This is the home page";
+var root = 
+"This is the root page";
+var restore = 
+"This is the restore page";
+var ice = 
+"This is the ice page";
+var hellfire = 
+"This is the hellfire page";
+var plasma = 
+"This is the plasma page";
+var extras = 
+"This is the extras page";
